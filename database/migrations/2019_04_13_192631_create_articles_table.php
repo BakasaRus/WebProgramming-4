9db8fea3a438bd14fb52e79441e07e84c966a711
Nick Andreev <bakasaru@list.ru>
2019-04-13 22:32:35 +0300

    Add schema in migration

--- database/migrations/2019_04_13_192631_create_articles_table.php
+++ database/migrations/2019_04_13_192631_create_articles_table.php
@@ -15,6 +15,8 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
