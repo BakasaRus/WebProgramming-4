1dc6f823fc2fe8ffc69121c35c852a515effe5e6
Nick Andreev <bakasaru@list.ru>
2019-04-14 00:32:57 +0300

    Add retrieving user's input after errors

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -22,12 +22,14 @@
             <div class="form-group">
                 <label for="title">Заголовок</label>
                 <input type="text"
-                       class="form-control" name="title" id="title" aria-describedby="titleHelp" placeholder="Lorem ipsum dolor sit amet.">
+                       class="form-control" name="title" id="title"
+                       aria-describedby="titleHelp" placeholder="Lorem ipsum dolor sit amet."
+                       value="{{ old('title') ?? $article->title ?? '' }}">
                 <small id="helpId" class="form-text text-muted">Тут нужно указать красивый и завлекающий заголовок</small>
             </div>
             <div class="form-group">
                 <label for="body">Текст статьи</label>
-                <textarea class="form-control" name="body" id="body" rows="7"></textarea>
+                <textarea class="form-control" name="body" id="body" rows="7">{{ old('body') ?? $article->body ?? '' }}</textarea>
             </div>
             <button type="submit" class="btn btn-primary">Создать</button>
         </form>
