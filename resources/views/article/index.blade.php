06c57fd96ebe6c88a65dd754885380ccbfebd445
Nick Andreev <bakasaru@list.ru>
2019-04-14 00:16:14 +0300

    Remove unnecessary container in index page

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -7,13 +7,11 @@
 @section('content')
     @foreach($articles as $article)
         <section class="my-5">
-            <div class="container">
-                <a href="{{ route('article.show', $article->id) }}">
-                    <h1>{{ $article->title }}</h1>
-                </a>
-                <p>{{ $article->body }}</p>
-                <p class="text-right">{{ $article->created_at->locale('ru')->diffForHumans() }}</p>
-            </div>
+            <a href="{{ route('article.show', $article->id) }}">
+                <h1>{{ $article->title }}</h1>
+            </a>
+            <p>{{ $article->body }}</p>
+            <p class="text-right">{{ $article->created_at->locale('ru')->diffForHumans() }}</p>
         </section>
     @endforeach
 @endsection
